a1086daa9e110d4ff210ef7fc1560e7e9a00662d
Antonio Hernandez <tonyhdezpi@gmail.com>
2024-08-05 18:31:39 -0600

    created a folder for custom hooks moved all the logic of the cart from app.jsx to its own file called useCart.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,19 @@
-import { useState, useEffect } from "react";
-import { db } from "./data/db";
 import "./App.css";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { useCart } from "./hooks/useCart";
 
 function App() {
-	// funcion para checar si tenemos algo en local storage y si si,
-	// ponerlo en el carrito, asi no perdemos esa informacion.
-	const initial = () => {
-		const savedCart = localStorage.getItem("cart");
-		return savedCart ? JSON.parse(savedCart) : [];
-	};
-	// eslint-disable-next-line no-unused-vars
-	const [data, setData] = useState(db);
-	const [cart, setCart] = useState(initial);
-
-	const MAX_ITEMS = 8;
-	const MIN_ITEMS = 1;
-
-	useEffect(() => {
-		localStorage.setItem("cart", JSON.stringify(cart));
-	}, [cart]);
-
-	function addToCart(item) {
-		const itemExist = cart.findIndex((guitar) => item.id === guitar.id);
-		if (itemExist < 0) {
-			item.quantity = 1;
-			setCart([...cart, item]);
-		} else {
-			if (cart[itemExist].quantity >= MAX_ITEMS) return;
-			const updatedCart = [...cart];
-			updatedCart[itemExist].quantity++;
-			setCart(updatedCart);
-		}
-	}
-
-	// siempre que le pases un callback al estado (setCart) sabra que que te refieres a el estado pasado
-	// entonces ahi podras trabajar sobre ese estado siempre y cuando no lo mutes, en este caso el filter no muta, sino que te devuelve una copia
-	function removeFromCart(id) {
-		// toma el estado pasado, le aplica un filtro y guarda en el nuevo estado todos los elementos que cumplan con la condicion
-		setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
-	}
-
-	function increaseQuantity(id) {
-		const updatedCart = cart.map((item) => {
-			if (item.id === id && item.quantity < MAX_ITEMS) {
-				return {
-					...item,
-					quantity: item.quantity + 1,
-				};
-			}
-			return item;
-		});
-		setCart(updatedCart);
-	}
-
-	function decreseQuantity(id) {
-		const updatedCart = cart.map((item) => {
-			if (item.id === id && item.quantity > MIN_ITEMS) {
-				return {
-					...item,
-					quantity: item.quantity - 1,
-				};
-			}
-			return item;
-		});
-		setCart(updatedCart);
-	}
-
-	function clearCart() {
-		setCart([]);
-	}
-
+	const {
+		data,
+		cart,
+		addToCart,
+		removeFromCart,
+		increaseQuantity,
+		decreseQuantity,
+		clearCart,
+	} = useCart();
+	console.log(data);
 	return (
 		<>
 			<Header
